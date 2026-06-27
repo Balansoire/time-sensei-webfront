4b597281853f5fcff415ddfefc5518656f2162ab
Théotime DUSERRE <98874870+Balansoire@users.noreply.github.com>
2026-06-27 16:09:03 +0200

    correct error

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,6 @@
 echo "Using API_URL=$API_URL"
 
 find /usr/share/nginx/html -type f -name "*.js" \
-  -exec sed -i "s|\${API_URL}|${$API_URL}|g" {} +
+  -exec sed -i "s|\${API_URL}|${API_URL}|g" {} +
 
 nginx -g "daemon off;"
